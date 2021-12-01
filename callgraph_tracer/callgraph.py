e011e4e98490c55efe516ff057c7c83930fb40b7
Lucas Atayde <42558249+lukeatayde@users.noreply.github.com>
2021-12-01 16:52:32 -0600

    Parsing each thread trace is now working.

--- callgraph_tracer/callgraph.py
+++ callgraph_tracer/callgraph.py
@@ -9,38 +9,39 @@ Script that parses and models call graph of program tracked by pintool.
 
 def parse_trace(filename: str):
 	file = codecs.open(filename, "r", encoding="cp1252", errors="replace")
-
-	line = file.readline()
-	 
-	# Find the start of each thread's function trace
-	while line and not re.match("Thread Function Trace: ", line):
-		line = file.readline()
-
-	if not line: 
-		return 
-
-	# At this point, we have found the head of a function trace.
-	# TODO: Thread numbers may not be unique.
-	thread = int(line.split(" ")[-1])
-	print(f"Found head for thread { thread }")
-
-	func_names = set()
 	func_name_re = re.compile("\t(?P<FunctionName>.*)$")
-
+	
 	line = file.readline()
-	while line:
-		match = func_name_re.search(line)
-		
-		# Hit a line that isn't a function call.
-		if not match:
-			break
+	while True:  
+		# Find the start of each thread's function trace
+		while line and not re.match("Thread Function Trace: ", line):
+			line = file.readline()
 
-		name = match.group("FunctionName")
-		if name not in func_names:
-			func_names.add(name)
+		if not line: 
+			return 
+
+		# At this point, we have found the head of a function trace.
+		# TODO: Thread numbers may not be unique.
+		thread = int(line.split(" ")[-1])
+		print(f"Found head for thread { thread }")
+
+		func_names = set()
 
 		line = file.readline()
+		while line:
+			match = func_name_re.search(line)
+		
+			# Hit a line that isn't a function call.
+			if not match:
+				break
 
-	print(func_names)
+			name = match.group("FunctionName")
+			if name not in func_names:
+				func_names.add(name)
+
+			line = file.readline()
+
+		# Hit the bottom of a thread's trace. Repeat the process until the file is exhausted.
+		print(func_names)
 
 parse_trace("calltrace_log.txt")
